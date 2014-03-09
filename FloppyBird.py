8d39a0a130b48916d60198eccc60cae1733b4ec6
James Jang <james@Bum-Ho.(none)>
2014-03-08 21:31:19 -0500

    pipe

--- FloppyBird.py
+++ FloppyBird.py
@@ -7,6 +7,8 @@ Created on Sat Mar  8 18:09:47 2014
 import pygame
 from pygame.locals import *
 
+from random import *
+
 class FloppyBird:
     
     def __init__(self):
@@ -22,7 +24,8 @@ class FloppyBird:
         pygame.display.set_caption('Floppy Bird')
         
         self.clock=pygame.time.Clock()
-        self.bird=Bird(self.screen)             
+        self.bird=Bird(self.screen)
+        self.pipe = Pipe(self.screen)        
         
     def update(self):
         self.clock.tick(60)
@@ -32,10 +35,17 @@ class FloppyBird:
             if event.type==QUIT:
                 exit()
             if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    exit()
                 if event.key == K_SPACE:
                     self.bird.jump()
+            if self.pipe.x == 0:
+                self.pipe.x = self.screen.get_width()
+                self.pipe.update()
+                
                 
         self.bird.update()
+        self.pipe.update()
         
         pygame.display.flip()
         
@@ -58,13 +68,32 @@ class Bird:
             self.yposition=self.screen.get_height()-self.radius
             
     def jump(self):
-        self.yvelocity=-10.0
+        self.yvelocity=-7.0
     
     def update(self):
         self.update_position()
         pygame.draw.circle(self.screen,(120,12,32),(int(self.xposition),int(self.yposition)),int(self.radius),0)
         
+class Pipe:
+    def __init__(self,screen):
+        self.screen=screen        
+        rand = random()
+        self.space = self.screen.get_height()/4.0*(random()*.3+.7)
         
+        self.x = self.screen.get_width()
+        self.y = 0  
+        self.length1 = self.screen.get_height()*rand
+        self.length2 = self.screen.get_height() - self.length1 - self.space
+        self.width = self.screen.get_width()/8.0
+    
+    def update_position(self):
+        self.x += -1.0
+        
+        
+    def update(self):
+        self.update_position()
+        pygame.draw.rect(self.screen, (255,255,255),pygame.Rect(self.x,self.y,self.width,self.length1-self.space))     
+        pygame.draw.rect(self.screen, (0,0,0),pygame.Rect(self.x ,(self.y+ self.length1+ self.space),self.width,self.length2))  
 
 if __name__ =='__main__':
     game = FloppyBird()
